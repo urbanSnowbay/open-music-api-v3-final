e8f7bb4c061269bdb0de33ad6bf720eb23224384
Haiqal Mochammad Satria Utama <haiqal231@gmail.com>
2022-03-10 23:23:38 +0700

    hapus comment

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable indent */
 /* Berkas ini menampung kode untuk membuat, mengonfigurasi, dan menjalankan HTTP server menggunakan Hapi. */
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
 
 const init = async () => {
+    const notesService = new NotesService();
+
     const server = Hapi.server({
         port: 5000,
-        host: 'localhost',
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: {
             cors: {
                 origin: ['*'],
@@ -15,7 +16,12 @@ const init = async () => {
         },
     });
 
-    server.route(routes);
+    await server.register({
+        plugin: notes,
+        options: {
+            service: notesService,
+        },
+    });
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
